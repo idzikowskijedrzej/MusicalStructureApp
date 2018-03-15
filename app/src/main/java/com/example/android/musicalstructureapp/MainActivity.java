a9b31576a305770432f2e3b33db736c8d6e37588
Jedrzej Idzikowski <33749192+idzikowskijedrzej@users.noreply.github.com>
2018-03-15 09:44:25 +0100

    Added android:descendantFocusability="blocksDescendants"
    It fixed all thoise empty clicks.

--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.android.musicalstructureapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
