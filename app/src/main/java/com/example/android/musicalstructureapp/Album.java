8fb164a1a222ead9ba1e8a964205a09fe7301be2
Jedrzej Idzikowski <33749192+idzikowskijedrzej@users.noreply.github.com>
2018-03-20 09:46:12 +0100

    meeting project spec

--- app/src/main/java/com/example/android/musicalstructureapp/Album.java
+++ app/src/main/java/com/example/android/musicalstructureapp/Album.java
@@ -3,26 +3,27 @@ package com.example.android.musicalstructureapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class Album extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_album1);
+        setContentView(R.layout.activity_album);
 
         //Find all view components
-        final ImageView imageView = findViewById(R.id.cover_image);
+        ImageView imageView = findViewById(R.id.cover_image);
         TextView topAlbumName = findViewById(R.id.topAlbumName);
         TextView bandTextView = findViewById(R.id.topArtistName);
         ListView trackList = findViewById(R.id.track_list);
+        TextView nowPlaying = findViewById(R.id.nowPlaying);
 
         //Collect extras
         Intent intent = getIntent();
@@ -38,41 +39,36 @@ public class Album extends AppCompatActivity {
 
         final ArrayList<String> myList = new ArrayList<>();
         addTracks(myList, getResources().getStringArray(R.array.trackList));
-        initAdapter(trackList,cover, myList, album, artist);
-    }
+        initAdapter(trackList, myList, album, nowPlaying);
 
-    private void initAdapter(ListView trackList, final int cover, final ArrayList<String> myList, final String album, final String artist){
-        trackList.setAdapter(new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, myList));
+    }
+    private void initAdapter(ListView trackList, final ArrayList<String> list, final String album, final TextView nowPlaying){
+        trackList.setAdapter(new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, list));
         trackList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Intent intent =new Intent (Album.this, DetailScreen.class);
-                intent.putExtra("position", myList.get(position));
-                intent.putExtra("cover", cover);
-                intent.putExtra("albumName", album);
-                intent.putExtra("artistName", artist);
-                startActivity(intent);
+                nowPlaying.setText(album + " - " +  list.get(position));
             }
         });
     }
-    private void addTracks(ArrayList<String>myList , String[] tracks){
-        myList.add(tracks[0]);
-        myList.add(tracks[1]);
-        myList.add(tracks[2]);
-        myList.add(tracks[3]);
-        myList.add(tracks[4]);
-        myList.add(tracks[5]);
-        myList.add(tracks[6]);
-        myList.add(tracks[7]);
-        myList.add(tracks[8]);
-        myList.add(tracks[9]);
-        myList.add(tracks[10]);
-        myList.add(tracks[11]);
-        myList.add(tracks[12]);
-        myList.add(tracks[13]);
-        myList.add(tracks[14]);
-        myList.add(tracks[15]);
-        myList.add(tracks[16]);
-        myList.add(tracks[17]);
+    private void addTracks(ArrayList<String>list , String[] tracks){
+        list.add(tracks[0]);
+        list.add(tracks[1]);
+        list.add(tracks[2]);
+        list.add(tracks[3]);
+        list.add(tracks[4]);
+        list.add(tracks[5]);
+        list.add(tracks[6]);
+        list.add(tracks[7]);
+        list.add(tracks[8]);
+        list.add(tracks[9]);
+        list.add(tracks[10]);
+        list.add(tracks[11]);
+        list.add(tracks[12]);
+        list.add(tracks[13]);
+        list.add(tracks[14]);
+        list.add(tracks[15]);
+        list.add(tracks[16]);
+        list.add(tracks[17]);
     }
 }
